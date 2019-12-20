4c92ae9ae9d79a24abfd142d6ef011d6859b9baf
Pascal Corpet <pcorpet@users.noreply.github.com>
2019-12-20 07:46:53 +0100

    Revert "Update the ROME importer, as the ROME v341 changed the format of the file. (#29)" (#30)
    This reverts commit 54485ea9b8b9393ab432ece83ecab51df9707d08.
    
    ROME v342 came back to the original format.

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ make -f "$DIR/Makefile"
 
 # Upload the names to Algolia.
 python "$DIR/upload.py" \
-  data/rome/csv/unix_referentiel_appellation_v*_utf8.csv \
-  data/rome/csv/unix_referentiel_code_rome_v*_utf8.csv \
+  data/rome/unix_referentiel_appellation_v*_utf8.csv \
+  data/rome/unix_referentiel_code_rome_v*_utf8.csv \
   data/passage_fap2009_romev3.txt \
   data/jobs_frequency.json
